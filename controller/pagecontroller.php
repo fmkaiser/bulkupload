930207574ed287d11361b7625ca7ab71ddd79bcc
Florian Kaiser <florian.kaiser@rzg.mpg.de>
2015-07-23 15:46:20 +0200

    first working prototype including auto-creation of /bulkupload folder

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -18,26 +18,20 @@ use OCP\AppFramework\Controller;
 
 class PageController extends Controller {
 
-
-	private $userId;
-
 	public function __construct($AppName, IRequest $request, $UserId){
 		parent::__construct($AppName, $request);
-		$this->userId = $UserId;
 	}
 
 	/**
-	 * CAUTION: the @Stuff turns off security checks; for this page no admin is
-	 *          required and no CSRF check. If you don't know what CSRF is, read
-	 *          it up in the docs or you might create a security hole. This is
-	 *          basically the only required method to add this exemption, don't
-	 *          add it to any other method if you don't exactly know what it does
-	 *
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
 	public function index() {
-		$params = ['user' => $this->userId];
+		$savePath = "/bulkupload";	
+		if (\OC\Files\Filesystem::file_exists($savePath) === false ){
+			$status = \OC\Files\Filesystem::mkdir($savePath);
+		}
+
 		return new TemplateResponse('bulkupload', 'main', $params);  // templates/main.php
 	}
 
