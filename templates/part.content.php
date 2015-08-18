a7eac5a7e8b4a59f70f472533cb2aa4a4600c678
Florian Kaiser <florian.kaiser@rzg.mpg.de>
2015-08-18 15:32:07 +0200

    encode "&" in html

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,6 +1,6 @@
 <div style="text-align: center; width: 600px;">
 	<div id="dropTarget" style="width:400px; height:300px; background-color: grey; padding: 50px; margin: 50px 100px; text-align: center; border-radius: 15px; -moz-border-radius: 15px; box-shadow: 10px 10px 5px #888888;">
-	  <a href="#" id="browseButton"><strong>Drag & drop files here</strong></a>
+	  <a href="#" id="browseButton"><strong>Drag &amp; drop files here</strong></a>
 	  <br/>
 	  <div id="filelist" style="width: 100%; text-align: center; overflow-y: scroll; height: 120px; "></div>
 	  <br/>
