a90c1c320f4ef29aa6bcea2b1b57d74696bd9c9f
Florian Kaiser <florian.kaiser@rzg.mpg.de>
2016-06-23 17:22:22 +0200

    add note about progress being stuck at 99% while the server reassembles the chunks

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,4 +1,5 @@
 <div style="margin: 50px 10px;">
 	By transparently splitting big files into smaller chunks, the bulk upload app allows you to upload files of an arbitrary size - as long as there is enough free space in your workspace. <br/><br/>
+	For big files, the progress bar will be stuck near 99% for a while while the server reassembles the chunks - please be patient.<br/><br/>
 	Powered by <a href="http://www.resumablejs.com" target="_blank">resumable.js</a>.
 </div>
